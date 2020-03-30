96ee059cbe85bd47d54674f2b7c676bb17889724
beishuo.wg <beishuo.wg@alibaba-inc.com>
2020-03-30 21:47:31 +0800

    update:增加括号匹配问题解法

--- src/main/java/org/example/StringChange.java
+++ src/main/java/org/example/StringChange.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 
 /**
@@ -23,6 +24,13 @@ public class StringChange {
         List<String> stringList = restoreIpAddresses(ipStr);
         for (String s : stringList) {
             System.out.println(s);
+        }
+
+        String strss = "(())(()))))()";
+        List<Integer> list = bracketsMatching(strss);
+        System.out.println("匹配数量：" + list.size());
+        for (Integer index : list) {
+            System.out.println("右括号深度：" + index);
         }
 
     }
@@ -223,4 +231,37 @@ public class StringChange {
         return result;
     }
 
+
+    //括号匹配问题 这个问题小米和头条都问题了
+    //下面的程序是我自己写的，可以去网上找找看其他的方法
+    //(())(()))))()
+    public static List<Integer> bracketsMatching(String s) {
+
+        if (s == null || s.length() == 0) {
+            return null;
+        }
+
+        List<Integer> list = new ArrayList<>();
+
+        int count = 0;
+        int index = 0;
+        char[] chars = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for (char aChar : chars) {
+            index++;
+
+            if ('(' == aChar) {
+                stack.add(aChar);
+            }
+
+            if (')' == aChar && !stack.empty()) {
+                stack.pop();
+                count++;
+                list.add(index);
+            }
+
+        }
+
+        return list;
+    }
 }
