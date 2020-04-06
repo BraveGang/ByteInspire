4f71a28ddb4b0e3b911c800696fecfbb41ec2c31
beishuo.wg <beishuo.wg@alibaba-inc.com>
2020-04-06 22:48:33 +0800

    update:AllOneSolution

--- src/main/java/org/problem/string/BracketsMatchingSolution.java
+++ src/main/java/org/problem/string/BracketsMatchingSolution.java
@@ -5,10 +5,14 @@ import java.util.List;
 import java.util.Stack;
 
 
-//括号匹配问题 这个问题小米和头条一面都问了
-//下面的程序是我自己写的，可以去网上找找看其他的方法
-//这个问题是多解法的-当前是使用左括号压栈的方式实现
-//(())(()))))()
+/**
+ *
+ * 括号匹配问题 这个问题小米和头条一面都问了
+ * 下面的程序是我自己写的，可以去网上找找看其他的方法
+ * 这个问题是多解法的-当前是使用左括号压栈的方式实现
+ * (())(()))))()
+ *
+ */
 public class BracketsMatchingSolution {
 
     public static void main(String[] args) {
